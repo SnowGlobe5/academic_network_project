86bbaaad5c6fa941ea1228bb22cf354efbc0cd66
= <SnowGlobe5>
2023-05-18 00:25:20 +0200

    Commit backup (errors)

--- anp_test.py
+++ anp_test.py
@@ -17,8 +17,8 @@ TOPIC = 2
 def get_papers_per_author_year(data, author, papers_year_list):
     edge_index = data['author', 'writes', 'paper'].edge_index
     sub_edge_index = expand_1_hop_edge_index(edge_index, author, flow='target_to_source')
-    mask = torch.isin(sub_edge_index[1], torch.tensor(papers_year_list).to('cuda:0'))
-    return sub_edge_index[:, mask][1].tolist()
+    mask = torch.isin(sub_edge_index[1], papers_year_list)
+    return sub_edge_index[:, mask][1]
 
 
 def expand_1_hop_edge_index(edge_index, node, flow):
@@ -31,7 +31,7 @@ def expand_1_hop_edge_index(edge_index, node, flow):
     return edge_index[:, mask]
 
 
-def expand_1_hop_graph(data, paths, last_expanded, list_node_to_expand):
+def expand_1_hop_graph_old(data, paths, last_expanded, list_node_to_expand):
     writes_edge_index = data['author', 'writes', 'paper'].edge_index
     cites_edge_index = data['paper', 'cites', 'paper'].edge_index
     about_edge_index = data['paper', 'about', 'topic'].edge_index
@@ -79,6 +79,49 @@ def expand_1_hop_graph(data, paths, last_expanded, list_node_to_expand):
 
     return new_last_expanded
 
+def expand_1_hop_graph(edge_index, nodes, type):
+
+    if type == PAPER:
+        if PAPER in list_node_to_expand:
+            sub_edge_index = expand_1_hop_edge_index(cites_edge_index, paper, flow='target_to_source')
+            for cited_paper in sub_edge_index[1].tolist():
+                if not paths[PAPER].get(cited_paper):
+                    paths[PAPER][cited_paper] = paths[PAPER][paper] + [('cites', [paper, cited_paper])]
+                    new_last_expanded[PAPER].append(cited_paper)
+
+        # co-authors
+        if AUTHOR in list_node_to_expand:
+            sub_edge_index = expand_1_hop_edge_index(writes_edge_index, paper, flow='source_to_target')
+            for co_author in sub_edge_index[0].tolist():
+                if not paths[AUTHOR].get(co_author):
+                    paths[AUTHOR][co_author] = paths[PAPER][paper] + [('writes', [co_author, paper])]
+                    new_last_expanded[AUTHOR].append(co_author)
+
+        # topic
+        if TOPIC in list_node_to_expand:
+            sub_edge_index = expand_1_hop_edge_index(about_edge_index, paper, flow='target_to_source')
+            for topic in sub_edge_index[1].tolist():
+                if not paths[TOPIC].get(topic):
+                    paths[TOPIC][topic] = paths[PAPER][paper] + [('about', [paper, topic])]
+                    new_last_expanded[TOPIC].append(topic)
+
+    for author in last_expanded[AUTHOR]:
+        if PAPER in list_node_to_expand:
+            sub_edge_index = expand_1_hop_edge_index(writes_edge_index, author, flow='target_to_source')
+            for paper in sub_edge_index[1].tolist():
+                if not paths[PAPER].get(paper):
+                    paths[PAPER][paper] = paths[AUTHOR][author] + [('writes', [author, paper])]
+                    new_last_expanded[PAPER].append(paper)
+
+    for topic in last_expanded[TOPIC]:
+        if PAPER in list_node_to_expand:
+            sub_edge_index = expand_1_hop_edge_index(about_edge_index, topic, flow='source_to_target')
+            for paper in sub_edge_index[0].tolist():
+                if not paths[PAPER].get(paper):
+                    paths[PAPER][paper] = paths[TOPIC][topic] + [('about', [paper, topic])]
+                    new_last_expanded[PAPER].append(paper)
+
+    return new_last_expanded
 
 def lazy_expand_graph(data, paths, last_expanded, node, expansion):
     if not last_expanded[1][node]:
@@ -206,11 +249,18 @@ def get_infosphere_old(data, data_next_year, author_id, paths, papers_next_year,
                             break
 
 
+
+
 def get_history_infosphere(data, data_next_year, author_id, papers_next_year):
     author_papers_next_year = get_papers_per_author_year(data_next_year, author_id, papers_next_year)
-    writes_edge_index = data_next_year['author', 'writes', 'paper'].edge_index
-    cites_edge_index = data_next_year['paper', 'cites', 'paper'].edge_index
-    about_edge_index = data_next_year['paper', 'about', 'topic'].edge_index
+    writes_edge_index_next_year = data_next_year['author', 'writes', 'paper'].edge_index
+    cites_edge_index_next_year = data_next_year['paper', 'cites', 'paper'].edge_index
+    about_edge_index_next_year = data_next_year['paper', 'about', 'topic'].edge_index
+
+    writes_edge_index = data['author', 'writes', 'paper'].edge_index
+    cites_edge_index = data['paper', 'cites', 'paper'].edge_index
+    about_edge_index = data['paper', 'about', 'topic'].edge_index
+
     infosphere = [{}, {}, {}]
 
     paper_user = set()
@@ -231,41 +281,58 @@ def get_history_infosphere(data, data_next_year, author_id, papers_next_year):
 
     sub_edge_index = expand_1_hop_edge_index(writes_edge_index, author_id, flow='target_to_source')
     author_papers = sub_edge_index[1]
-    for paper in author_papers.tolist():
+    for paper in author_papers:
         # cited papers
         sub_edge_index = expand_1_hop_edge_index(cites_edge_index, paper, flow='target_to_source')
-        mask = torch.isin(sub_edge_index[1], torch.tensor(papers_next_year).to('cuda:0'), invert=True)
-        for cited_paper in sub_edge_index[:, mask][1].tolist():
+        for cited_paper in sub_edge_index[1]:
             paper_user.add(cited_paper)
 
         # co-authors
         sub_edge_index = expand_1_hop_edge_index(writes_edge_index, paper, flow='source_to_target')
         mask = sub_edge_index[0] != author_id
-        for co_author in sub_edge_index[:, mask][0].tolist():
+        for co_author in sub_edge_index[:, mask][0]:
             author_user.add(co_author)
 
         # topic
         sub_edge_index = expand_1_hop_edge_index(about_edge_index, paper, flow='target_to_source')
-        for topic in sub_edge_index[1].tolist():
+        for topic in sub_edge_index[1]:
             topic_user.add(topic)
 
     for paper in author_papers_next_year:
         # cited papers
-        sub_edge_index = expand_1_hop_edge_index(cites_edge_index, paper, flow='target_to_source')
-        mask = torch.isin(sub_edge_index[1], torch.tensor(papers_next_year).to('cuda:0'), invert=True)
-        for cited_paper in sub_edge_index[:, mask][1].tolist():
+        sub_edge_index = expand_1_hop_edge_index(cites_edge_index_next_year, paper, flow='target_to_source')
+        mask = torch.isin(sub_edge_index[1], papers_next_year, invert=True)
+        for cited_paper in sub_edge_index[:, mask][1]:
             paper_seed.add(cited_paper)
 
         # co-authors
-        sub_edge_index = expand_1_hop_edge_index(writes_edge_index, paper, flow='source_to_target')
+        sub_edge_index = expand_1_hop_edge_index(writes_edge_index_next_year, paper, flow='source_to_target')
         mask = sub_edge_index[0] != author_id
-        for co_author in sub_edge_index[:, mask][0].tolist():
+        for co_author in sub_edge_index[:, mask][0]:
             author_seed.add(co_author)
 
         # topic
-        sub_edge_index = expand_1_hop_edge_index(about_edge_index, paper, flow='target_to_source')
-        for topic in sub_edge_index[1].tolist():
+        sub_edge_index = expand_1_hop_edge_index(about_edge_index_next_year, paper, flow='target_to_source')
+        for topic in sub_edge_index[1]:
             topic_seed.add(topic)
+
+    while paper_seed and author_seed and topic_seed:
+        paper_user_scanned.update(paper_user)
+        author_user_scanned.update(author_user)
+        topic_user_scanned.update(topic_user)
+        # expand
+        paper_user.difference_update(paper_user_scanned)
+        author_user.difference_update(author_user_scanned)
+        topic_user.difference_update(topic_user_scanned)
+
+        append frontier-user to scanned-user
+frontier-user=expansion of (frontier-user) - scanned-user
+compare frontiers and remove seeds found
+
+append frontier-seed to scanned-infosphere
+frontier-seed=expansion of (frontier-user) - scanned-infosphere
+compare frontiers and remove seeds found
+
 
 
 def main():
@@ -284,14 +351,17 @@ def main():
     sub_graph.to('cuda:0')
     sub_graph_next_year.to('cuda:0')
 
+    tensor_paper_next_year = torch.tensor(papers_next_year).to('cuda:0')
+
     time = datetime.now()
     for i, author in enumerate(history_author_list):
         if author == 74662:
             time = datetime.now()
-            get_history_infosphere(sub_graph, sub_graph_next_year, i, papers_next_year)
+            get_history_infosphere(sub_graph, sub_graph_next_year, i, tensor_paper_next_year)
             print(f"Infosphere creation time: {str(datetime.now() - time)}")
     print(f"History & Infosphere creation time for a fold: {str(datetime.now() - time)}")
 
 
 if __name__ == "__main__":
-    cProfile.run('main()')
+    #cProfile.run('main()')
+    main()
