b0d00cccebd38c09fc8b0096fcd98538d433be30
= <SnowGlobe5>
2023-05-29 22:59:22 +0200

    Reformat & expansion

--- anp_expansion.py
+++ anp_expansion.py
@@ -1,29 +1,75 @@
 import torch
 import random
 
+from anp_utils import *
+
 # once the paths have been computed  you can  create the actual infosphere by using an extension policy. the base  policy would start from the pahts and choose randomly
 # a node and randomly expand a few children. No need to check for duplicates. the only parameters would be how many nodes to expand and the maximum children node to expand.
 
 # please keep node selection and children expansions part of the policy separated as they can be later specialised by learning or by taking into account domain specific
 # heuristics like open the most cited papers etc
 
+def select_random_index(extracted_nodes):
+    lenght = len(extracted_nodes)
+    if lenght > 0:
+        return random.randrange(lenght)
+
 def random_selection_policy(node_list):
     node_type = random.randrange(3)
     list_selected = node_list[node_type]
     element_index = random.randrange(len(list_selected))
-    return list_selected[element_index]
+    return list_selected[element_index], node_type
 
-def random_expansion_policy(node, n_children, edge_list):
-    # edge_list = torch.cat((infosphere_edge_list[ABOUT], element[1]), dim=1) 
-    pass
+def random_expansion_policy(node, node_type, n_children, node_list, edge_list):
+    for i in range(n_children):
+        if node_type == PAPER:
+            edge_type_to_expand = node_type_to_expand = random.randrange(3)
+            
+            match edge_type_to_expand:
+                # CITES
+                case 0:
+                    sub_edge_index, _ = expand_1_hop_edge_index(edge_list[CITES], node, flow='target_to_source')
+                    position = 1
+                    
+                # WRITES
+                case 1:
+                    sub_edge_index, _ = expand_1_hop_edge_index(edge_list[WRITES], node, flow='source_to_target')
+                    position = 0
+                # ABOUT
+                case 2:
+                    sub_edge_index, _ = expand_1_hop_edge_index(edge_list[ABOUT], node, flow='target_to_source')
+                    position = 1
+                    
+        elif node_type == AUTHOR:
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_list[WRITES], node, flow='target_to_source')
+            position = 1
+            edge_type_to_expand = WRITES
+            node_type_to_expand = PAPER
+            
+        elif node_type == TOPIC:
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_list[ABOUT], node, flow='source_to_target')
+            position = 0
+            edge_type_to_expand = ABOUT
+            node_type_to_expand = PAPER
+        
+    index_node = select_random_index(sub_edge_index[position])
+    if index_node:
+        mask = sub_edge_index[position] == sub_edge_index[position][index_node]
+        edge_list[edge_type_to_expand] = torch.cat((edge_list[edge_type_to_expand], sub_edge_index[:, mask]), dim=1)
+        node_list[node_type_to_expand] = torch.cat((node_list[node_type_to_expand], sub_edge_index[position][index_node]), dim=1)
+        
+    return True
 
 # Check node_list is not empty before call expand_infosphere
 def expand_infosphere(node_list, edge_list, n_node, n_children, selection_policy, expansion_policy):
     if node_list == [[], [], []]: 
         raise Exception("Not possible to expand an empty infosphere.")
     
-    count = 0
-    while count == n_node:
-        node = selection_policy(node_list)
-        if expansion_policy(node, n_children, edge_list):
-            count += 1
+    count_node_expanded = 0
+    retry = 3
+    while count_node_expanded < n_node or retry == 0:
+        node, node_type = selection_policy(node_list)
+        if expansion_policy(node, node_type, n_children, node_list, edge_list):
+            count_node_expanded += 1
+        else:
+            retry -= 1
---
+++ anp_utils.py
@@ -0,0 +1,77 @@
+PAPER = 0
+AUTHOR = 1
+TOPIC = 2
+
+CITES = 0
+WRITES = 1
+ABOUT = 2
+
+MAX_ITERATION = 1
+DEVICE = 'cuda:1'
+
+
+def expand_1_hop_edge_index(edge_index, node, flow):
+    # _, sub_edge_index, _, _ = k_hop_subgraph(node, 1, edge_index, flow=flow)
+    # Clean
+    if flow == 'target_to_source':
+        mask = edge_index[0] == node
+    else:
+        mask = edge_index[1] == node
+    return edge_index[:, mask], mask
+
+
+def expand_1_hop_graph(edge_index, nodes, type, paths):
+    if type == PAPER:
+        paper_nodes = []
+        author_nodes = []
+        topic_nodes = []
+        for paper in nodes:
+            # cited paper
+            # sub_edge_index, _ = expand_1_hop_edge_index(edge_index[0], paper, flow='target_to_source')
+            # for cited_paper in sub_edge_index[1].tolist():
+            #     if not paths[PAPER].get(cited_paper):
+            #         paper_nodes.append(cited_paper)
+            #         paths[PAPER][cited_paper] = paths[PAPER][paper] + [('cites', [paper, cited_paper])]
+            
+            # Since this is used from seed we are intrested in papers that cites it
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_index[CITES], paper, flow='source_to_target')
+            for cited_paper in sub_edge_index[0].tolist():
+                if not paths[PAPER].get(cited_paper):
+                    paper_nodes.append(cited_paper)
+                    paths[PAPER][cited_paper] = paths[PAPER][paper] + [('cites', [cited_paper, paper])]
+
+            # co-authors
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_index[WRITES], paper, flow='source_to_target')
+            for co_author in sub_edge_index[0].tolist():
+                if not paths[AUTHOR].get(co_author):
+                    author_nodes.append(co_author)
+                    paths[AUTHOR][co_author] = paths[PAPER][paper] + [('writes', [co_author, paper])]
+
+            # topic
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_index[ABOUT], paper, flow='target_to_source')
+            for topic in sub_edge_index[1].tolist():
+                if not paths[TOPIC].get(topic):
+                    topic_nodes.append(topic)
+                    paths[TOPIC][topic] = paths[PAPER][paper] + [('about', [paper, topic])]
+
+        return (paper_nodes, author_nodes, topic_nodes)
+
+    elif type == AUTHOR:
+        paper_nodes = []
+        for author in nodes:
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_index, author, flow='target_to_source')
+            for paper in sub_edge_index[1].tolist():
+                if not paths[PAPER].get(paper):
+                    paper_nodes.append(paper)
+                    paths[PAPER][paper] = paths[AUTHOR][author] + [('writes', [author, paper])]
+        return paper_nodes
+
+    else:
+        paper_nodes = []
+        for topic in nodes:
+            sub_edge_index, _ = expand_1_hop_edge_index(edge_index, topic, flow='source_to_target')
+            for paper in sub_edge_index[0].tolist():
+                if not paths[PAPER].get(paper):
+                    paper_nodes.append(paper)
+                    paths[PAPER][paper] = paths[TOPIC][topic] + [('about', [paper, topic])]
+        return paper_nodes
