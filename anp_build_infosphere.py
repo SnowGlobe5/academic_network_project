505465ad946e5c25949f298e8e2fc52ecbaabbfd
SnowGlobe5 <SnowGlobe5>
2023-09-20 11:53:07 +0200

    Update

--- anp_build_infosphere.py
+++ anp_build_infosphere.py
@@ -4,7 +4,7 @@ import os
 import re
 
 
-def build_infosphere(fold, year):
+def build_infosphere(fold, year, number):
     DEVICE=torch.device(f'cuda:1' if torch.cuda.is_available() else 'cpu')
     authors_infosphere_edge_list = [
             torch.tensor([[],[]]).to(torch.int64).to(DEVICE),
@@ -17,7 +17,10 @@ def build_infosphere(fold, year):
     fold_string = '_'.join(fold_string)
     
     regex = re.compile(f'^infosphere_{fold_string}_{year}_noisy_.*\.pt$')
-
+    
+    if not os.path.exists(f"{rootdir}/fragments_{number}"):
+        os.makedirs(f"{rootdir}/fragments_{number}")
+    
     for root, dirs, files in os.walk(rootdir):
         for file in files:
             if regex.match(file):
@@ -26,7 +29,9 @@ def build_infosphere(fold, year):
                 authors_infosphere_edge_list[WRITES] = torch.cat((authors_infosphere_edge_list[WRITES], author_edge_list[WRITES].to(DEVICE)), dim=1)
                 authors_infosphere_edge_list[ABOUT] = torch.cat((authors_infosphere_edge_list[ABOUT], author_edge_list[ABOUT].to(DEVICE)), dim=1)
 
-            
-    torch.save(authors_infosphere_edge_list, f"ANP_DATA/computed_infosphere/infosphere_{fold_string}_{year}_noisy_p15_p25_p35_f2.pt")
+                os.rename(f"{rootdir}/{file}", f"{rootdir}/fragments_{number}/{file}")
+        break    
+    torch.save(authors_infosphere_edge_list, f"ANP_DATA/computed_infosphere/{number}_infosphere_{fold_string}_{year}_noisy.pt")
    
-build_infosphere([0, 1, 2, 3, 4], 2019)                 
+argument = sys.argv[1]
+build_infosphere([0, 1, 2, 3, 4], 2019, argument)                 
