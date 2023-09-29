6bb418d7cf586761565371a8e125f60a75343ec9
SnowGlobe5 <SnowGlobe5>
2023-09-29 11:23:51 +0200

    Add runners

--- runner_anp_link_prediction.sh
+++ runner_anp_link_prediction.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python3 anp_link_prediction_co_author_infosphere.py False 1
-python3 anp_link_prediction_co_author_infosphere.py False 2
-python3 anp_link_prediction_co_author_infosphere.py False 3
-python3 anp_link_prediction_co_author_infosphere.py False 4
-python3 anp_link_prediction_co_author_infosphere.py False 5
+#python3 anp_link_prediction_co_author_infosphere_no_exp.py True
+python3 anp_link_prediction_co_author_no_infosphere.py False 0.00001
+python3 anp_link_prediction_co_author_no_infosphere.py False 0.000001
+
+python3 anp_link_prediction_co_author_infosphere.py False 0.00001 1
+python3 anp_link_prediction_co_author_infosphere.py False 0.000001 1
--- runner_noisy_infosphere.sh
+++ runner_noisy_infosphere.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
+python3 noisy-caller.py 1 > noisy1.log
+python3 anp_build_infosphere.py 1
+
 python3 noisy-caller.py 2 > noisy2.log
-
-while true; do
-  if [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_0_0_1.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_0_0_2.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_1_1_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_2_3_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_4_4_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_5_9_None.pt ]
-  then
-      break
-  fi
-  sleep 60
-done
-
 python3 anp_build_infosphere.py 2
 
+python3 noisy-caller.py 3 > noisy3.log
+python3 anp_build_infosphere.py 3
 
 python3 noisy-caller.py 4 > noisy4.log
-
-while true; do
-  if [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_0_0_1.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_0_0_2.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_1_1_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_2_3_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_4_4_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_5_9_None.pt ]
-  then
-      break
-  fi
-  sleep 60
-done
-
 python3 anp_build_infosphere.py 4
 
-
 python3 noisy-caller.py 5 > noisy5.log
-
-while true; do
-  if [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_0_0_1.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_0_0_2.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_1_1_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_2_3_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_4_4_None.pt ] && [ -e ANP_DATA/computed_infosphere/infosphere_0_1_2_3_4_2019_noisy_5_9_None.pt ]
-  then
-      break
-  fi
-  sleep 60
-done
-
 python3 anp_build_infosphere.py 5
+
+
+python3 anp_link_prediction_co_author_infosphere.py False 1
+python3 anp_link_prediction_co_author_infosphere.py False 2
+python3 anp_link_prediction_co_author_infosphere.py False 3
+python3 anp_link_prediction_co_author_infosphere.py False 4
+python3 anp_link_prediction_co_author_infosphere.py False 5
