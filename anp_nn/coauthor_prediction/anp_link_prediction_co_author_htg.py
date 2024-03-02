3f6c5dffa35c20bc23845088a97ea387be43ff8b
Sabrina Guidotti <s.guidotti2@campus.unimib.it>
2024-03-02 23:29:52 +0100

    Fix HTG (not finished yet)

--- anp_nn/coauthor_prediction/anp_link_prediction_co_author_htg.py
+++ anp_nn/coauthor_prediction/anp_link_prediction_co_author_htg.py
@@ -6,9 +6,12 @@ import torch.nn.functional as F
 import torch_geometric.transforms as T
 from torch_geometric.datasets import DBLP
 from torch_geometric.nn import HGTConv, Linear
+from torch_geometric.loader import LinkNeighborLoader
+from torch_geometric.data import DataLoader
 
-from academic_network_project.anp_core.anp_dataset import ANPDataset
-from academic_network_project.anp_core.anp_utils import *
+from anp_dataset import ANPDataset
+from anp_utils import *
+from tqdm import tqdm
 
 # path = osp.join(osp.dirname(osp.realpath(__file__)), '../../data/DBLP')
 # # We initialize conference node features with a single one-vector as feature:
@@ -19,49 +22,71 @@ from academic_network_project.anp_core.anp_utils import *
 BATCH_SIZE = 4096
 YEAR = 2019
 
-ROOT = "../anp_data"
+ROOT = "ANP_DATA"
 
-DEVICE=torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+DEVICE=torch.device('cuda')
 
 # Create ANP dataset
 dataset = ANPDataset(root=ROOT)
 data = dataset[0]
-data = anp_simple_filter_data(data, ROOT, [0], 2019)
+y = torch.randint(0, 4, (data["author"].num_nodes,), dtype=torch.long)
+data["author"].y = y
+
+# Use already existing co-author edge (if exist)
+if os.path.exists(f"{ROOT}/processed/co_author_edge{YEAR+1}.pt"):
+    print("Co-author edge found!")
+    data['author', 'co_author', 'author'].edge_index = torch.load(f"{ROOT}/processed/co_author_edge{YEAR+1}.pt")
+    data['author', 'co_author', 'author'].edge_label = None
+else:
+    print("Generating co-author edge...")
+    data['author', 'co_author', 'author'].edge_index = generate_co_author_edge_year(data, YEAR+1)
+    data['author', 'co_author', 'author'].edge_label = None
+    torch.save(data['author', 'co_author', 'author'].edge_index, f"{ROOT}/processed/co_author_edge{YEAR+1}.pt")
+
 
 # Make paper features float and the graph undirected
 data['paper'].x = data['paper'].x.to(torch.float)
 data = T.ToUndirected()(data)
-data = data.to(DEVICE)
+data = data.to('cpu')
 
+sub_graph_train= anp_simple_filter_data(data, root=ROOT, folds=[0, 1, 2, 3, 4], max_year=YEAR)    
+ 
+transform = T.RandomLinkSplit(
+    num_val=0.2,
+    num_test=0.0,
+    #disjoint_train_ratio=0.3,
+    neg_sampling_ratio=1.0,
+    add_negative_train_samples=True,
+    edge_types=('author', 'co_author', 'author')
+)
+train_data, val_data, _= transform(sub_graph_train)
 
-def create_masks(data, train_ratio=0.8, val_ratio=0.05, test_ratio=0.15):
-    num_authors = data.num_nodes
-    num_train = int(num_authors * train_ratio)
-    num_val = int(num_authors * val_ratio)
-    num_test = num_authors - num_train - num_val
-    
-    # Genera gli indici casuali degli autori
-    indices = torch.randperm(num_authors)
-    
-    # Crea le maschere
-    train_mask = torch.zeros(num_authors, dtype=torch.bool)
-    val_mask = torch.zeros(num_authors, dtype=torch.bool)
-    test_mask = torch.zeros(num_authors, dtype=torch.bool)
-    
-    # Assegna gli indici agli insiemi di train, validation e test
-    train_mask[indices[:num_train]] = True
-    val_mask[indices[num_train:num_train+num_val]] = True
-    test_mask[indices[num_train+num_val:]] = True
-    
-    return train_mask, val_mask, test_mask
+# Define seed edges:
+edge_label_index = train_data['author', 'co_author', 'author'].edge_label_index
+edge_label = train_data['author', 'co_author', 'author'].edge_label
+train_loader = LinkNeighborLoader(
+    data=train_data,
+    num_neighbors=[20, 10],
+    #neg_sampling_ratio=2.0,
+    edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
+    edge_label=edge_label,
+    batch_size=1024,
+    shuffle=True,
+)
 
-# Usa la funzione per creare le maschere
-train_mask, val_mask, test_mask = create_masks(data)
+edge_label_index = val_data['author', 'co_author', 'author'].edge_label_index
+edge_label = val_data['author', 'co_author', 'author'].edge_label
+val_loader = LinkNeighborLoader(
+    data=val_data,
+    num_neighbors=[20, 10],
+    edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
+    edge_label=edge_label,
+    batch_size=1024,
+    shuffle=False,
+)
 
-embedding_author = torch.nn.Embedding(data["author"].num_nodes, 32).to(DEVICE)
-embedding_topic = torch.nn.Embedding(data["topic"].num_nodes, 32).to(DEVICE)
-data['author'].x = embedding_author(data['author'].id.long())
-data['topic'].x = embedding_topic(data['topic'].id.long())
+del data['author', 'co_author', 'author']
+
 
 class HGT(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, num_heads, num_layers):
@@ -69,7 +94,6 @@ class HGT(torch.nn.Module):
 
         self.lin_dict = torch.nn.ModuleDict()
         for node_type in data.node_types:
-            print(node_type)
             self.lin_dict[node_type] = Linear(-1, hidden_channels)
 
         self.convs = torch.nn.ModuleList()
@@ -95,38 +119,53 @@ model = HGT(hidden_channels=64, out_channels=4, num_heads=2, num_layers=1)
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(DEVICE)
 
-with torch.no_grad():  # Initialize lazy modules.
-    out = model(data.x_dict, data.edge_index_dict)
+# with torch.no_grad():  # Initialize lazy modules.
+#     out = model(data.x_dict, data.edge_index_dict)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
 
+embedding_author = torch.nn.Embedding(data["author"].num_nodes, 32).to(DEVICE)
+embedding_topic = torch.nn.Embedding(data["topic"].num_nodes, 32).to(DEVICE)
 
 def train():
     model.train()
-    optimizer.zero_grad()
-    out = model(data.x_dict, data.edge_index_dict)
-    print (train_mask, out[train_mask], data['author'].y[train_mask])
-    loss = F.cross_entropy(out[train_mask], data['author'].y[train_mask])
-    loss.backward()
-    optimizer.step()
-    return float(loss)
+    total_examples = total_correct = total_loss = 0
+    for i, batch in enumerate(tqdm(train_loader)):
+        batch = batch.to(DEVICE)
+        del batch['author', 'co_author', 'author']
+        
+        # Add user node features for message passing:
+        batch['author'].x = embedding_author(batch['author'].n_id)
+        batch['topic'].x = embedding_topic(batch['topic'].n_id)
+        
+        optimizer.zero_grad()
+        out = model(batch.x_dict, batch.edge_index_dict)
+        loss = F.cross_entropy(out, batch["author"].y)
+        loss.backward()
+        optimizer.step()
+        total_loss += float(loss) * out.numel()
+        total_examples += out.numel()
+        out = out.clamp(min=0, max=3)
+        # total_correct += int((torch.round(out, decimals=0) == batch['author'].y).sum())
+    return total_loss
 
 
-@torch.no_grad()
-def test():
-    model.eval()
-    pred = model(data.x_dict, data.edge_index_dict).argmax(dim=-1)
+# @torch.no_grad()
+# def test():
+#     model.eval()
+#     pred = model(data.x_dict, data.edge_index_dict).argmax(dim=-1)
 
-    accs = []
-    for split in (train_mask, val_mask, test_mask):
-        mask = data['author'][split]
-        acc = (pred[mask] == data['author'].y[mask]).sum() / mask.sum()
-        accs.append(float(acc))
-    return accs
+#     accs = []
+#     for split in (train_mask, val_mask, test_mask):
+#         mask = data['author'][split]
+#         acc = (pred[mask] == data['author'].y[mask]).sum() / mask.sum()
+#         accs.append(float(acc))
+#     return accs
 
 
 for epoch in range(1, 101):
     loss = train()
-    train_acc, val_acc, test_acc = test()
-    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
-          f'Val: {val_acc:.4f}, Test: {test_acc:.4f}')
+    print(loss)
+    # train_acc, val_acc, test_acc = test()
+    # print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
+    #       f'Val: {val_acc:.4f}, Test: {test_acc:.4f}')
