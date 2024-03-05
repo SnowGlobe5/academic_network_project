b7066660ba7304cf1d60a444b1c8ffb3134afade
sguidotti2001@gmail.com <sguidotti2001@gmail.com>
2024-03-05 09:48:07 +0100

    Fix HGT

--- anp_nn/coauthor_prediction/anp_link_prediction_co_author_htg.py
+++ anp_nn/coauthor_prediction/anp_link_prediction_co_author_htg.py
@@ -1,36 +1,38 @@
-import os.path as osp
-
 import torch
+import os
 import torch.nn.functional as F
-
 import torch_geometric.transforms as T
-from torch_geometric.datasets import DBLP
-from torch_geometric.nn import HGTConv, Linear
+from academic_network_project.anp_core.anp_dataset import ANPDataset
+from academic_network_project.anp_core.anp_utils import *
+from torch.nn import Linear
 from torch_geometric.loader import LinkNeighborLoader
-from torch_geometric.data import DataLoader
-
-from anp_dataset import ANPDataset
-from anp_utils import *
+from torch_geometric.nn import SAGEConv, HGTConv, to_hetero
 from tqdm import tqdm
-
-# path = osp.join(osp.dirname(osp.realpath(__file__)), '../../data/DBLP')
-# # We initialize conference node features with a single one-vector as feature:
-# dataset = DBLP(path, transform=T.Constant(node_types='conference'))
-# data = dataset[0]
-# print(data)
 
 BATCH_SIZE = 4096
 YEAR = 2019
 
-ROOT = "ANP_DATA"
+ROOT = "../anp_data"
+PATH = "../anp_models/1_co_author_prediction_future_hgt/"
 
-DEVICE=torch.device('cuda')
+DEVICE=torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+
+if sys.argv[1] == 'True':
+    use_link_split = True
+else:
+    use_link_split = False
+lr = float(sys.argv[2])
+    
+# #TODO remove
+# import shutil
+# try:
+#     shutil.rmtree(PATH)
+# except:
+#     pass
 
 # Create ANP dataset
 dataset = ANPDataset(root=ROOT)
 data = dataset[0]
-y = torch.randint(0, 4, (data["author"].num_nodes,), dtype=torch.long)
-data["author"].y = y
 
 # Use already existing co-author edge (if exist)
 if os.path.exists(f"{ROOT}/processed/co_author_edge{YEAR+1}.pt"):
@@ -41,89 +43,187 @@ else:
     print("Generating co-author edge...")
     data['author', 'co_author', 'author'].edge_index = generate_co_author_edge_year(data, YEAR+1)
     data['author', 'co_author', 'author'].edge_label = None
-    torch.save(data['author', 'co_author', 'author'].edge_index, f"{ROOT}/processed/co_author_edge{YEAR+1}.pt")
-
+    torch.save(data['author', 'co_author', 'author'].edge_index, f"{ROOT}/processed/co_author_edge{YEAR*1}.pt")
 
 # Make paper features float and the graph undirected
 data['paper'].x = data['paper'].x.to(torch.float)
 data = T.ToUndirected()(data)
-data = data.to('cpu')
 
-sub_graph_train= anp_simple_filter_data(data, root=ROOT, folds=[0, 1, 2, 3, 4], max_year=YEAR)    
+if use_link_split == True:
+    sub_graph_train= anp_simple_filter_data(data, root=ROOT, folds=[0, 1, 2, 3, 4], max_year=YEAR)    
  
-transform = T.RandomLinkSplit(
-    num_val=0.2,
-    num_test=0.0,
-    #disjoint_train_ratio=0.3,
-    neg_sampling_ratio=1.0,
-    add_negative_train_samples=True,
-    edge_types=('author', 'co_author', 'author')
-)
-train_data, val_data, _= transform(sub_graph_train)
+    transform = T.RandomLinkSplit(
+        num_val=0.2,
+        num_test=0.0,
+        #disjoint_train_ratio=0.3,
+        neg_sampling_ratio=1.0,
+        add_negative_train_samples=True,
+        edge_types=('author', 'co_author', 'author')
+    )
+    train_data, val_data, _= transform(sub_graph_train)
 
-# Define seed edges:
-edge_label_index = train_data['author', 'co_author', 'author'].edge_label_index
-edge_label = train_data['author', 'co_author', 'author'].edge_label
-train_loader = LinkNeighborLoader(
-    data=train_data,
-    num_neighbors=[20, 10],
-    #neg_sampling_ratio=2.0,
-    edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
-    edge_label=edge_label,
-    batch_size=1024,
-    shuffle=True,
-)
+    # Define seed edges:
+    edge_label_index = train_data['author', 'co_author', 'author'].edge_label_index
+    edge_label = train_data['author', 'co_author', 'author'].edge_label
+    train_loader = LinkNeighborLoader(
+        data=train_data,
+        num_neighbors=[20, 10],
+        #neg_sampling_ratio=2.0,
+        edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
+        edge_label=edge_label,
+        batch_size=1024,
+        shuffle=True,
+    )
 
-edge_label_index = val_data['author', 'co_author', 'author'].edge_label_index
-edge_label = val_data['author', 'co_author', 'author'].edge_label
-val_loader = LinkNeighborLoader(
-    data=val_data,
-    num_neighbors=[20, 10],
-    edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
-    edge_label=edge_label,
-    batch_size=1024,
-    shuffle=False,
-)
+    edge_label_index = val_data['author', 'co_author', 'author'].edge_label_index
+    edge_label = val_data['author', 'co_author', 'author'].edge_label
+    val_loader = LinkNeighborLoader(
+        data=val_data,
+        num_neighbors=[20, 10],
+        edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
+        edge_label=edge_label,
+        batch_size=1024,
+        shuffle=False,
+    )
+else:
+    # Train
+    # Filter training data
+    sub_graph_train, _, _, _ = anp_filter_data(data, root=ROOT, folds=[0, 1, 2, 3 ], max_year=YEAR, keep_edges=False)    
+    #sub_graph_train = sub_graph_train.to(DEVICE)
 
+    transform = T.RandomLinkSplit(
+        num_val=0,
+        num_test=0,
+        #disjoint_train_ratio=0.3,
+        #neg_sampling_ratio=2.0,
+        neg_sampling_ratio=1.0,
+        add_negative_train_samples=True,
+        edge_types=('author', 'co_author', 'author')
+    )
+    train_data, _, _= transform(sub_graph_train)
+
+
+    # Validation
+    # Filter validation data
+    sub_graph_val= anp_simple_filter_data(data, root=ROOT, folds=[4], max_year=YEAR)  
+    #sub_graph_val = sub_graph_val.to(DEVICE)
+
+    transform = T.RandomLinkSplit(
+        num_val=0,
+        num_test=0,
+        #neg_sampling_ratio=2.0,
+        neg_sampling_ratio=1.0,
+        add_negative_train_samples=True,
+        edge_types=('author', 'co_author', 'author')
+    )
+    val_data, _, _= transform(sub_graph_val)
+
+
+    # Define seed edges:
+    edge_label_index = train_data['author', 'co_author', 'author'].edge_label_index
+    edge_label = train_data['author', 'co_author', 'author'].edge_label
+    train_loader = LinkNeighborLoader(
+        data=train_data,
+        num_neighbors=[20, 10],
+        #neg_sampling_ratio=2.0,
+        edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
+        edge_label=edge_label,
+        batch_size=1024,
+        shuffle=True,
+    )
+
+    edge_label_index = val_data['author', 'co_author', 'author'].edge_label_index
+    edge_label = val_data['author', 'co_author', 'author'].edge_label
+    val_loader = LinkNeighborLoader(
+        data=val_data,
+        num_neighbors=[20, 10],
+        edge_label_index=(('author', 'co_author', 'author'), edge_label_index),
+        edge_label=edge_label,
+        batch_size=1024,
+        shuffle=False,
+    )
+
+# Delete the co-author edge (data will be used for data.metadata())
 del data['author', 'co_author', 'author']
 
+# Initialize weight
+weight = None
 
-class HGT(torch.nn.Module):
-    def __init__(self, hidden_channels, out_channels, num_heads, num_layers):
+
+def weighted_mse_loss(pred, target, weight=None):
+    weight = 1. if weight is None else weight[target].to(pred.dtype)
+    return (weight * (pred - target.to(pred.dtype)).pow(2)).mean()
+
+
+class GNNEncoder(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels, num_heads, num_layers, metadata):
         super().__init__()
 
+        self.hidden_channels = hidden_channels
         self.lin_dict = torch.nn.ModuleDict()
         for node_type in data.node_types:
-            self.lin_dict[node_type] = Linear(-1, hidden_channels)
+            print(node_type)
+            self.lin_dict[node_type] = Linear(hidden_channels, hidden_channels)
 
         self.convs = torch.nn.ModuleList()
         for _ in range(num_layers):
-            conv = HGTConv(hidden_channels, hidden_channels, data.metadata(),
-                           num_heads)
+            conv = HGTConv(hidden_channels, hidden_channels, metadata,
+                            num_heads)
             self.convs.append(conv)
 
         self.lin = Linear(hidden_channels, out_channels)
 
     def forward(self, x_dict, edge_index_dict):
-        x_dict = {
-            node_type: self.lin_dict[node_type](x).relu_()
-            for node_type, x in x_dict.items()
-        }
+        for node_type, x in x_dict.items():
+            # Get the input size dynamically
+            input_size = x.size(1)
+            self.lin_dict[node_type] = torch.nn.Linear(input_size, self.hidden_channels).to(DEVICE)
+
+            x_dict[node_type] = self.lin_dict[node_type](x).relu_()
         for conv in self.convs:
             x_dict = conv(x_dict, edge_index_dict)
 
-        return self.lin(x_dict['author'])
+        return x_dict
 
 
-model = HGT(hidden_channels=64, out_channels=4, num_heads=2, num_layers=1)
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = model.to(DEVICE)
+class EdgeDecoder(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.lin1 = Linear(2 * hidden_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, 1)
+    def forward(self, z_dict, edge_label_index):
+        row, col = edge_label_index
+        z = torch.cat([z_dict['author'][row], z_dict['author'][col]], dim=-1)
 
-# with torch.no_grad():  # Initialize lazy modules.
-#     out = model(data.x_dict, data.edge_index_dict)
+        z = self.lin1(z).relu()
+        z = self.lin2(z)
+        return z.view(-1)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
 
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.encoder = GNNEncoder(hidden_channels, hidden_channels, 2, 1, data.metadata())
+        # self.encoder = to_hetero(self.encoder, data.metadata(), aggr='sum')
+        self.decoder = EdgeDecoder(hidden_channels)
+
+    def forward(self, x_dict, edge_index_dict, edge_label_index):
+        z_dict = self.encoder(x_dict, edge_index_dict)
+        return self.decoder(z_dict, edge_label_index)
+
+
+# Create model, optimizer, and move model to device
+# If exist load last checkpoint
+# if os.path.exists(PATH):
+#     model, first_epoch = anp_load(PATH)
+#     first_epoch += 1
+# else:
+model = Model(hidden_channels=32).to(DEVICE)
+os.makedirs(PATH)
+with open(PATH + 'info.json', 'w') as json_file:
+    json.dump([], json_file)
+first_epoch = 1
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 embedding_author = torch.nn.Embedding(data["author"].num_nodes, 32).to(DEVICE)
 embedding_topic = torch.nn.Embedding(data["topic"].num_nodes, 32).to(DEVICE)
 
@@ -132,40 +232,105 @@ def train():
     total_examples = total_correct = total_loss = 0
     for i, batch in enumerate(tqdm(train_loader)):
         batch = batch.to(DEVICE)
+        
+        edge_label_index = batch['author', 'author'].edge_label_index
+        edge_label = batch['author', 'author'].edge_label
         del batch['author', 'co_author', 'author']
         
         # Add user node features for message passing:
         batch['author'].x = embedding_author(batch['author'].n_id)
         batch['topic'].x = embedding_topic(batch['topic'].n_id)
-        
+
         optimizer.zero_grad()
-        out = model(batch.x_dict, batch.edge_index_dict)
-        loss = F.cross_entropy(out, batch["author"].y)
+        pred = model(batch.x_dict, batch.edge_index_dict, edge_label_index)
+        target = edge_label
+        loss = F.binary_cross_entropy_with_logits(pred, target)
         loss.backward()
         optimizer.step()
-        total_loss += float(loss) * out.numel()
-        total_examples += out.numel()
-        out = out.clamp(min=0, max=3)
-        # total_correct += int((torch.round(out, decimals=0) == batch['author'].y).sum())
-    return total_loss
+        total_loss += float(loss) * pred.numel()
+        total_examples += pred.numel()
+        pred = pred.clamp(min=0, max=1)
+        total_correct += int((torch.round(pred, decimals=0) == target).sum())
+
+    return total_correct/total_examples, total_loss / total_examples
 
 
-# @torch.no_grad()
-# def test():
-#     model.eval()
-#     pred = model(data.x_dict, data.edge_index_dict).argmax(dim=-1)
+@torch.no_grad()
+def test(loader):
+    model.eval()
+    total_examples = total_correct = total_loss = 0
+    for i, batch in enumerate(tqdm(loader)):
+        batch = batch.to(DEVICE)
+        
+        edge_label_index = batch['author', 'author'].edge_label_index
+        edge_label = batch['author', 'author'].edge_label
+        del batch['author', 'co_author', 'author']
+        
+        # Add user node features for message passing:
+        batch['author'].x = embedding_author(batch['author'].n_id)
+        batch['topic'].x = embedding_topic(batch['topic'].n_id)
 
-#     accs = []
-#     for split in (train_mask, val_mask, test_mask):
-#         mask = data['author'][split]
-#         acc = (pred[mask] == data['author'].y[mask]).sum() / mask.sum()
-#         accs.append(float(acc))
-#     return accs
+        pred = model(batch.x_dict, batch.edge_index_dict, edge_label_index)
+        target = edge_label
+        loss = F.binary_cross_entropy_with_logits(pred, target)
+        total_loss += float(loss) * pred.numel()
+        total_examples += pred.numel()
+        pred = pred.clamp(min=0, max=1)
+        total_correct += int((torch.round(pred, decimals=0) == target).sum())
+
+        for i in range(len(target)):
+            if target[i].item() == 0:
+                if torch.round(pred, decimals=0)[i].item() == 0:
+                    confusion_matrix['tn'] += 1
+                else:
+                    confusion_matrix['fn'] += 1
+            else:
+                if torch.round(pred, decimals=0)[i].item() == 1:
+                    confusion_matrix['tp'] += 1
+                else:
+                    confusion_matrix['fp'] += 1
+
+    return total_correct / total_examples, total_loss / total_examples
 
 
-for epoch in range(1, 101):
-    loss = train()
-    print(loss)
-    # train_acc, val_acc, test_acc = test()
-    # print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
-    #       f'Val: {val_acc:.4f}, Test: {test_acc:.4f}')
+# Initialize optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+training_loss_list = []
+validation_loss_list = []
+training_accuracy_list = []
+validation_accuracy_list = []
+confusion_matrix = {
+    'tp': 0,
+    'fp': 0,
+    'fn': 0,
+    'tn': 0
+}
+
+for epoch in range(first_epoch, 41):
+    # Train the model
+    train_acc, train_loss = train()
+
+    confusion_matrix = {
+        'tp': 0,
+        'fp': 0,
+        'fn': 0,
+        'tn': 0
+    }
+        
+    # Test the model
+    val_acc, loss_val = test(val_loader)
+
+    # Save the model
+    anp_save(model, PATH, epoch, train_loss, loss_val, val_acc)
+    
+    training_loss_list.append(train_loss)
+    validation_loss_list.append(loss_val)
+    training_accuracy_list.append(train_acc)
+    validation_accuracy_list.append(val_acc)
+    
+    # Print epoch results
+    print(f'Epoch: {epoch:02d}, Loss: {train_loss:.4f} - {loss_val:.4f}, Accuracy: {val_acc:.4f}')
+    if epoch == 31:
+        generate_graph (training_loss_list, validation_loss_list, training_accuracy_list, validation_accuracy_list, confusion_matrix)
+generate_graph (training_loss_list, validation_loss_list, training_accuracy_list, validation_accuracy_list, confusion_matrix)
